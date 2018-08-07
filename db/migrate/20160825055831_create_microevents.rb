a8b002affc2ff614c3cf852f2807c397926b1551
Mike Meisels <mmeisels@salesforce.com>
2018-08-07 17:15:47 +1000

    Deleted rake

--- db/migrate/20160825055831_create_microevents.rb
+++ db/migrate/20160825055831_create_microevents.rb
@@ -1,4 +1,4 @@
-class CreateMicroevents < ActiveRecord::Migration
+class CreateMicroevents < ActiveRecord::Migration[4.2]
   def change
     create_table :microevents do |t|
       t.text :description
